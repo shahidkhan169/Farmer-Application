514dae395b49522bc9b8b0c1ee3de60022a35361
Naveen_Kumar <naveennaveengeetha2606@gmail.com>
2024-08-21 00:19:49 +0530

    naveen

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { useState } from 'react';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
@@ -6,6 +5,9 @@ import Details from './components/Details';
 import Details1 from './components/Details1';
 import Details2 from './components/Details2';
 import Product from './components/Product';
+import HomePage from './components/HomePage';
+import RegistrationPage from './components/RegistrationPage';
+import FarmerDashboard from './components/FarmerDashboard'; // Import your new dashboard component
 
 function App() {
 
@@ -17,27 +19,16 @@ function App() {
           <Route path='/details' element={<Details1/>}/>
           <Route path='/details2' element={<Details2/>}/>
           <Route path='/product' element={<Product/>}/>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register/:type" element={<RegistrationPage />} />
+          <Route path="/dashboard" element={<FarmerDashboard />} /> {/* Add the new route */}
         </Routes>
       </BrowserRouter>
     </div>
-=======
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import RegistrationPage from './components/RegistrationPage';
-import FarmerDashboard from './components/FarmerDashboard'; // Import your new dashboard component
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register/:type" element={<RegistrationPage />} />
-        <Route path="/dashboard" element={<FarmerDashboard />} /> {/* Add the new route */}
-      </Routes>
-    </Router>
->>>>>>> d643c47c5795aa6bd21affdfbb95d07bc2809840
-  );
+  )
 }
+
+
+
 
 export default App;
--- frontend/frontend/tailwind.config.js
+++ frontend/frontend/tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-=======
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
->>>>>>> d643c47c5795aa6bd21affdfbb95d07bc2809840
   theme: {
     extend: {},
   },
