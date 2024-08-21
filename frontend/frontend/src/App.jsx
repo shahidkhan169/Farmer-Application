eab350a2fc479355039ea6374519512a42232caf
Sathiyaseelan S <sathiyaseelan0712@gmail.com>
2024-08-21 09:00:06 +0530

    Frontend Integrated

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,9 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 import { useState } from 'react';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
-import Details from './components/Details';
-import Details1 from './components/Details1';
-import Details2 from './components/Details2';
+import SignUpFarmer from './pages/signupfarmer'
 import Product from './components/Product';
 import HomePage from './components/HomePage';
 import RegistrationPage from './components/RegistrationPage';
@@ -15,9 +14,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/*' element={<Details/>}/>
-          <Route path='/details' element={<Details1/>}/>
-          <Route path='/details2' element={<Details2/>}/>
+          <Route path='/register/farmer' element={<SignUpFarmer/>}/>
           <Route path='/product' element={<Product/>}/>
           <Route path="/" element={<HomePage />} />
           <Route path="/register/:type" element={<RegistrationPage />} />
